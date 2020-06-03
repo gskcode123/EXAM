e751e8f0f2f0931df9ba375409c35a708070b06a
gskcode123 <ganeshmrr12@gmail.com>
2020-06-03 19:28:57 -0400

    update7

--- AFexam/settings.py
+++ AFexam/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'viif0&pttf63m@+*kkwrxrwrt&xcmys5xlp0@da@z_m5x6r&!&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
